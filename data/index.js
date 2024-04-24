448ad2199a4aa00dbfbaf17421d0ff705d782af8
SpicyMcSpice1938 <badmd67@gmail.com>
2024-04-24 15:35:56 -0400

    trades data functions and seeding

--- data/index.js
+++ data/index.js
@@ -2,10 +2,10 @@ import pepperDataFunctions from "./peppers.js";
 import userDataFunctions from "./users.js";
 import usersPeppersDataFunctions from "./userspeppers.js";
 import commentDataFunctions from "./comments.js";
-// import tradeDataFunctions from "./trades.js";
+import tradeDataFunctions from "./trades.js";
 
 export const peppersData = pepperDataFunctions;
 export const usersData = userDataFunctions;
-// export const tradesData = tradeDataFunctions;
+export const tradesData = tradeDataFunctions;
 export const usersPeppersData = usersPeppersDataFunctions;
 export const commentsData = commentDataFunctions;
